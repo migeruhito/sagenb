b74c1ffd496c94eed93c68907e3c542b191e3940
J. Miguel Farto <migeruhito@gmail.com>
2015-12-13 18:15:57 +0100

    history management removed from User class

--- sagenb/notebook/user.py
+++ sagenb/notebook/user.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*
 from __future__ import absolute_import
 
-import copy
-import cPickle
 import hashlib
 import random
-import os
 
-# TODO: sage dependency
-from sage.misc.temporary_file import atomic_write
-
-from .. import config
 from .conf_models import UserConfiguration_from_basic
 from .conf_models import UserConfiguration
-
-SALT = 'aa'
 
 
 def User_from_basic(basic):
@@ -66,20 +57,14 @@ class User(object):
         else:
             return True
 
-    def __getstate__(self):
-        d = copy.copy(self.__dict__)
+    def __repr__(self):
+        return self._username
 
-        # Some old worksheets have this attribute, which we do *not* want to
-        # save.
-        if 'history' in d:
-            try:
-                self.save_history()
-                del d['history']
-            except Exception, msg:
-                print msg
-                print("Unable to dump history of user %s to disk yet." %
-                      self._username)
-        return d
+    def __getitem__(self, *args):
+        return self._conf.__getitem__(*args)
+
+    def __setitem__(self, *args):
+        self._conf.__setitem__(*args)
 
     def basic(self):
         """
@@ -90,40 +75,6 @@ class User(object):
                   for x, y in self.__dict__.iteritems() if x[0] == '_'])
         d['conf'] = self._conf.basic()
         return d
-
-    def history_list(self):
-        try:
-            return self.history
-        except AttributeError:
-            if config.notebook is None:
-                return []
-            history_file = "%s/worksheets/%s/history.sobj" % (
-                config.notebook.directory(), self._username)
-            if os.path.exists(history_file):
-                try:
-                    self.history = cPickle.load(open(history_file))
-                except:
-                    print "Error loading history for user %s" % self._username
-                    self.history = []
-            else:
-                self.history = []
-            return self.history
-
-    def save_history(self):
-        if not hasattr(self, 'history'):
-            return
-        if config.notebook is None:
-            return
-        history_file = "%s/worksheets/%s/history.sobj" % (
-            config.notebook.directory(), self._username)
-        try:
-            # print "Dumping %s history to '%s'"%(self.__username,
-            # history_file)
-            his = cPickle.dumps(self.history)
-        except AttributeError:
-            his = cPickle.dumps([])
-        with atomic_write(history_file) as f:
-            f.write(his)
 
     def username(self):
         """
@@ -153,9 +104,6 @@ class User(object):
         """
         return self._password
 
-    def __repr__(self):
-        return self._username
-
     def conf(self):
         """
         EXAMPLES::
@@ -174,12 +122,6 @@ class User(object):
             False
         """
         return self._conf
-
-    def __getitem__(self, *args):
-        return self._conf.__getitem__(*args)
-
-    def __setitem__(self, *args):
-        self._conf.__setitem__(*args)
 
     def set_password(self, password, encrypt=True):
         """
