b919e6b787960a4d217fd1a83277efbabb615cfa
J Miguel Farto <migeruhito@gmail.com>
2016-06-23 19:01:38 +0200

    backport: Update Sagenb to 0.12: properly package sagenb

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@
 
 import os
 from setuptools import setup
-import distutils.log
 
 
 
@@ -31,51 +30,49 @@ def all_files(dir, prefix):
     return X
 
 
-install_requires = [ 'twisted>=11.0.0'
-                   , 'flask>=0.10.1'
-                   , 'flask-oldsessions>=0.10'
-                   , 'flask-openid'
-                   , 'flask-autoindex'
-                   , 'babel'
-                   , 'flask-babel'
-                   , 'webassets'
-                   , 'flask-themes2'
-                   ]
+install_requires = [
+    'twisted>=11.0.0',
+    'flask>=0.10.1',
+    'flask-oldsessions>=0.10',
+    'flask-openid',
+    'flask-autoindex',
+    'flask-babel'
+    'flask-themes2',
+    ]
 
-if __name__ == '__main__':
-    if os.environ.get("SAGE_SETUPTOOLS_DEBUG","no")=="yes":
-        distutils.log.set_threshold(distutils.log.DEBUG)
 
-    code = setup(name = 'sagenb',
-          version     = '0.11.4',
-          description = 'The Sage Notebook',
-          license     = 'GNU General Public License (GPL) v3+',
-          author      = 'William Stein et al.',
-          author_email= 'sage-notebook@googlegroups.com',
-          url         = 'http://github.com/sagemath/sagenb',
-          install_requires = install_requires,
-          dependency_links =    [
-                                  'http://github.com/mitsuhiko/flask-oldsessions/tarball/master#egg=flask-oldsessions-0.10'
-                                ],
-          test_suite = 'sagenb.testing.run_tests.all_tests',
-          packages    = [ 'sagenb'
-                        , 'sagenb.flask_version'
-                        , 'sagenb.interfaces'
-                        , 'sagenb.misc'
-                        , 'sagenb.notebook'
-                        , 'sagenb.notebook.compress'
-                        , 'sagenb.simple'
-                        , 'sagenb.storage'
-                        , 'sagenb.testing'
-                        , 'sagenb.testing.tests'
-                        , 'sagenb.testing.selenium'
-                        ],
-          scripts      = [ 'sagenb/data/sage3d/sage3d',
-                         ],
-          package_data = {'sagenb':
-                              all_files('sagenb/data', 'sagenb/') +
-                              all_files('sagenb/translations', 'sagenb/') +
-                              all_files('sagenb/themes', 'sagenb/')
-                         },
-          zip_safe     = False,
-          )
+setup(
+    name='sagenb',
+    version='0.12',
+    description='The Sage Notebook',
+    license='GNU General Public License (GPL) v3+',
+    author='William Stein et al.',
+    author_email='sage-notebook@googlegroups.com',
+    url='http://github.com/sagemath/sagenb',
+    install_requires=install_requires,
+    dependency_links=[
+        'http://github.com/mitsuhiko/flask-oldsessions/tarball/'
+        'master#egg=flask-oldsessions-0.10'
+        ],
+    test_suite='sagenb.testing.run_tests.all_tests',
+    packages=[
+        'sagenb',
+        'sagenb.flask_version',
+        'sagenb.interfaces',
+        'sagenb.misc',
+        'sagenb.notebook',
+        'sagenb.notebook.compress',
+        'sagenb.simple',
+        'sagenb.storage',
+        'sagenb.testing',
+        'sagenb.testing.tests',
+        'sagenb.testing.selenium',
+        ],
+    scripts=['sagenb/data/sage3d/sage3d'],
+    package_data={
+        'sagenb': (all_files('sagenb/data', 'sagenb/') +
+                   all_files('sagenb/translations', 'sagenb/') +
+                   all_files('sagenb/themes', 'sagenb/'))
+        },
+    zip_safe=False,
+)
